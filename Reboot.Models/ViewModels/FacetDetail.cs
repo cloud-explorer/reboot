9b1a2d44753ac1f5fad47b22a6c930e3edd9583f
rrangan <rrangan@gmail.com>
2014-06-16 14:23:04 -0400

    Added Search Params

---
+++ Reboot.Models/ViewModels/FacetDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projects.Reboot.Core.Search;
+
+namespace Projects.Models.ViewModels
+{
+    public class FacetDetail
+    {
+        public FacetCategory Category { get; set; }
+
+        public FacetValue Value { get; set; }
+    }
+}
